6cda65a106e74077be8a8170e58d42c340457392
birukbelaytafese <birukbelay175@gmail.com>
2026-05-16 14:28:27 -0500

    completed lease contract and phase 1

--- src/main/java/com/yearup/dealership/LeaseContract.java
+++ src/main/java/com/yearup/dealership/LeaseContract.java
@@ -1,4 +1,39 @@
+
 package com.yearup.dealership;
 
-public class LeaseContract {
+public class LeaseContract extends Contract {
+
+    private double expectedEndingValue;
+    private double leaseFee;
+    public LeaseContract(String date, String customerName, String customerEmail,
+                         Vehicle vehicleSold) {
+
+        super(date, customerName, customerEmail, vehicleSold);
+    }@Override
+    public double getTotalPrice() {
+
+        double price = getVehicleSold().getPrice();
+
+        expectedEndingValue = price * 0.5;
+        leaseFee = price * 0.07;
+
+        return price + leaseFee;
+    }
+    @Override
+    public double getMonthlyPayment() {
+
+        double leasePrice = getTotalPrice() - expectedEndingValue;
+
+        double monthlyInterestRate = 0.04 / 12;
+        int months = 36;
+
+        return (leasePrice * monthlyInterestRate) /
+                (1 - Math.pow(1 + monthlyInterestRate, -months));
+    }public double getExpectedEndingValue() {
+        return expectedEndingValue;
+    }
+
+    public double getLeaseFee() {
+        return leaseFee;
+    }
 }
