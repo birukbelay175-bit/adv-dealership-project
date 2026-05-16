80c8e7adcf7e65722b160e47499fbae8264ea896
birukbelaytafese <birukbelay175@gmail.com>
2026-05-16 14:24:09 -0500

    completed contract class

--- src/main/java/com/yearup/dealership/Contract.java
+++ src/main/java/com/yearup/dealership/Contract.java
@@ -1,4 +1,39 @@
 package com.yearup.dealership;
 
 public class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+
+    }
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    public double getMonthlyPayment() {
+        return 0;
+    }
 }
--- src/main/java/com/yearup/dealership/SalesContract.java
+++ src/main/java/com/yearup/dealership/SalesContract.java
@@ -1,4 +1,61 @@
 package com.yearup.dealership;
 
-public class SalesContract {
+public class SalesContract extends Contract {
+
+    private double salesTaxAmount;
+    private double recordingFee;
+    private double processingFee;
+    private boolean finance;
+    public SalesContract(String date, String customerName, String customerEmail,
+                         Vehicle vehicleSold, boolean finance) {
+
+        super(date, customerName, customerEmail, vehicleSold);
+
+        this.finance = finance;
+    }
+    @Override
+    public double getTotalPrice() {
+
+        double price = getVehicleSold().getPrice();
+
+        salesTaxAmount = price * 0.05;
+        recordingFee = 100;
+
+        if (price < 10000) {
+            processingFee = 295;
+        } else {
+            processingFee = 495;
+        }
+
+        return price + salesTaxAmount + recordingFee + processingFee;
+    }
+    @Override
+    public double getMonthlyPayment() {
+
+        if (!finance) {
+            return 0;
+        }
+
+        double totalPrice = getTotalPrice();
+
+        if (getVehicleSold().getPrice() >= 10000) {
+
+            double monthlyInterestRate = 0.0425 / 12;
+            int months = 48;
+
+            return (totalPrice * monthlyInterestRate) /
+                    (1 - Math.pow(1 + monthlyInterestRate, -months));
+
+        } else {
+
+            double monthlyInterestRate = 0.0525 / 12;
+            int months = 24;
+
+            return (totalPrice * monthlyInterestRate) /
+                    (1 - Math.pow(1 + monthlyInterestRate, -months));
+        }
+    }
+    public boolean isFinance() {
+        return finance;
+    }
 }
